0a7196054feff1f012f5336f9b8fda1154131fb9
Ostap Letnyanchyk <ostap.letnyanchyk@gmail.com>
2020-10-27 15:50:32 +0200

    add attributs

--- wp-content/themes/enfold-child/includes/woocommerce_custom_attribute.php
+++ wp-content/themes/enfold-child/includes/woocommerce_custom_attribute.php
@@ -10,13 +10,45 @@ function my_template_loop_product_title(){
     $height = $product->get_height();
     $lengthArrt = $product->get_attribute('razmer-mesta-dlinna');
 
-    $custom_attribute = '<div class="d-flex">';
-    $custom_attribute .= '<div>' . $lengthArrt . '</div> x ';
-    // $custom_attribute .= '<div>' . $width . '</div> x ';
-    // $custom_attribute .= '<div>' . $height . '</div>';
+    $custom_attribute  = '<div class="product-attributes-wrap">';
+    $custom_attribute .= '<div class="product-attributes-list">';
+    $custom_attribute .= '<div class="product-attributes-item">';
+    $custom_attribute .= '<div class="product-attributes-title"><span>Размер</span></div>';
+    $custom_attribute .= '<div class="product-size-table d-flex">';
+    $custom_attribute .= '<div class="product-size product-length">';
+    $custom_attribute .= '<div class="product-size-title">длина</div>';
+    $custom_attribute .= '<div class="product-size-value">'. $length .'</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '<div class="product-size product-width">';
+    $custom_attribute .= '<div class="product-size-title">ширина</div>';
+    $custom_attribute .= '<div class="product-size-value">'. $width .'</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '<div class="product-size product-height">';
+    $custom_attribute .= '<div class="product-size-title">высота</div>';
+    $custom_attribute .= '<div class="product-size-value">'. $height .'</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '<div class="product-attributes-item">';
+    $custom_attribute .= '<div class="product-attributes-title"><span>Размер спального места</span></div>';
+    $custom_attribute .= '<div class="product-size-table d-flex">';
+    $custom_attribute .= '<div class="product-size product-length">';
+    $custom_attribute .= '<div class="product-size-title">длина</div>';
+    $custom_attribute .= '<div class="product-size-value">12</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '<div class="product-size product-width">';
+    $custom_attribute .= '<div class="product-size-title">ширина</div>';
+    $custom_attribute .= '<div class="product-size-value">13</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '<div class="product-size product-height">';
+    $custom_attribute .= '<div class="product-size-title">высота</div>';
+    $custom_attribute .= '<div class="product-size-value">14</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '</div>';
+    $custom_attribute .= '</div>';
     $custom_attribute .= '</div>';
 
-    
     echo  $custom_attribute;
 
 }
