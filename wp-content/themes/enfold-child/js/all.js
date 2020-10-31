1130b6add96b5aa1d78bf5396995782a78b02120
Ostap Letnyanchyk <ostap.letnyanchyk@gmail.com>
2020-10-31 22:51:21 +0200

    js

--- wp-content/themes/enfold-child/js/all.js
+++ wp-content/themes/enfold-child/js/all.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function() {
     let inner_product =  jQuery('.inner_product');
-        inner_product_h = jQuery('.inner_product .product-attributes-wrap').height();
+        inner_product_a = jQuery('.inner_product .product-attributes-wrap').height();
+        inner_product_h = inner_product_a + 20;
         inner_product.find('.product-box-shasow').css( "height", "calc(100% + " + inner_product_h +  "px)");  
         return;
+
 });
 
